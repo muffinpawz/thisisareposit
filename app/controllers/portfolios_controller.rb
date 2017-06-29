c3c50489c139ad2a8760b576e75e557f7a8628b7
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 16:53:52 +0000

    Added a delete feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -42,5 +42,19 @@ class PortfoliosController < ApplicationController
  @portfolio_item = Portfolio.find(params[:id]) 
   end
   
+  def destroy
+    # Perform lookup
+        @portfolio_item = Portfolio.find(params[:id]) 
+       
+       # Destroy the post 
+      @portfolio_item.destroy
+      
+      #Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully deleted.' }
+    end
+  end
+
+  
 end
 
