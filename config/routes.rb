c001e144ca637bcd4db410f86b1435836d53784d
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 14:28:43 +0000

    Stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'portfolios/index'
+
   resources :portfolios
   get 'pages/home'
 
--- test/controllers/portfolios_controller_test.rb
+++ test/controllers/portfolios_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class PortfoliosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get portfolios_index_url
+    assert_response :success
+  end
+
 end
