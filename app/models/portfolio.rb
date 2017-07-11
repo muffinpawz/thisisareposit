2a7b8b8dcdbf1355a2ceb07e2f873f77c53802fc
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 14:51:55 +0000

    Implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,4 @@
 class Portfolio < ApplicationRecord
-<<<<<<< HEAD
   validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
   
  def self.fmab
@@ -13,11 +12,8 @@ class Portfolio < ApplicationRecord
  after_initialize :set_defaults
  
  def set_defaults
-   self.main_image ||= "http://placehold.it/600x400"
-   self.thumb_image ||="https://placehold.it/350x200"
+   self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+   self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
  end
-=======
-  validates_presence_of :title, :body, :main_image, :thumb_image
->>>>>>> data-feature
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706164307) do
+ActiveRecord::Schema.define(version: 20170711143027) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "slug"
-    t.integer  "status"
+    t.integer  "status",     default: 0
     t.integer  "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
     t.index ["topic_id"], name: "index_blogs_on_topic_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170706164307) do
     t.integer  "percent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "badge"
   end
 
   create_table "topics", force: :cascade do |t|
