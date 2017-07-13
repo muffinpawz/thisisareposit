bf34574bb1562743734f2caa24e15e5fbcc71825
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 15:11:04 +0000

    More commiting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path: '', path_names: { sign_in: 'login',
-  sign_out: 'logout', sign_up: 'register' }
+  
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get "portfolios/ylia", to: 'portfolios#ylia'
   get "portfolios/fmab", to: 'portfolios#fmab'
