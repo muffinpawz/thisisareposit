d78f83793012cb73bce0a2472d7f67d279e40d81
Jordan Hudgens <jordan@devcamp.com>
2017-08-08 14:09:05 +0000

    Pls

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog
+  
+  validates :content, presence: ture, length: {minium: 5, maximiun: 1000 }
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,6 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
   
-  
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
   
